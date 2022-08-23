d0d2046e31a45c9aeecf5af53b6580470c36b1c0
alpapie <alpapie0908@gmail.com>
2022-08-23 01:23:27 +0000

    first commit for python code

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,4 +2,5 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
 language that combines remarkable power with very clear syntax"
 str=str[39:66] +str[105:111] + str[:6]
+
 print(str)
