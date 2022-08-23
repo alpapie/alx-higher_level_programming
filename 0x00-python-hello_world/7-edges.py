e3f7462671ebb499b5d57c5ce5baac5741fa92eb
alpapie <alpapie0908@gmail.com>
2022-08-23 01:30:30 +0000

    first commit for python code

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 word = "Holberton"
-print(f"First 3 letters: {word[:3]}")
-print(f"Last 2 letters: {word[-2:]}")
-print(f"Middle word: {word[1:8]}\n")
+print(f"First 3 letters: { word[:3] }")
+print(f"Last 2 letters: { word[-2:] }")
+print(f"Middle word: { word[1:8] }")
