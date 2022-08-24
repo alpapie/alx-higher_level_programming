d019d56a8fbda40b9881c702654a656c25d71306
alpapie <alpapie0908@gmail.com>
2022-08-24 20:56:35 +0000

    if else

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-# print the strig in uppercase with ord() and chr()
-
+#!/usr/bin/python3
 def uppercase(str):
-    mot=""
-    for char in str :
-        if ord(char) >= 97 and ord(char) <= 122 :
-            char = chr(ord(char) - 32)
-        else:
-            char = char
-        print("{}".format(char), end="")
-uppercase("Holberton IS the bESt")
+    for car in str:
+        if 97 <= ord(car) <= 122:
+            car = chr(ord(car) - 32)
+        print("{}".format(car), end="")
+    print()
