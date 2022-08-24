e2be26df813fc387ae89e071343178d295522868
alpapie <alpapie0908@gmail.com>
2022-08-24 18:08:49 +0000

    if else

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-#greater than 5, is zero, is less than 6 and not 0
 
 if number < 0:
         last_digit = number % -10
@@ -14,4 +13,3 @@ else:
                 print("Last digit of {} is {} and is 0".format(number, last_digit))
         elif last_digit < 6 and last_digit != 0:
                 print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit)) 
-
--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 for alphabet in range(97, 123):
     print("{}".format(chr(alphabet)), end="")
-
