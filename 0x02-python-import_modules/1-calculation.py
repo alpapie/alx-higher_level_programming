5a5ece95ba7831c6510552bae8eaa3d5748bdcb0
alpapie <alpapie0908@gmail.com>
2022-08-25 23:50:38 +0000

    alpapie python module

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    from calculator_1 import add, sub, mul, div
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+
+def main():
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
+
+if __name__ == "__main__":
+    main()
