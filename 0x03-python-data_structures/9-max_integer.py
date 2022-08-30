7af3f0d37de5d6c10d3b8a2d887420bf74e43d3b
alpapie <alpapie0908@gmail.com>
2022-08-30 14:37:05 +0000

    def print_list_integer(my_list=[]):

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if(len(my_list) is 0):
+    if(len(my_list) == 0):
         return None
     else:
-        return max(my_list)
+        max_int = my_list[0]
+        for i in my_list:
+            if(i >= max_int):
+                max_int = i
+    return max_int
