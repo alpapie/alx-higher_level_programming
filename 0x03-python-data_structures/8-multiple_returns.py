d62e9a4003e63b954459462d0e18ef4f4248aa3e
alpapie <alpapie0908@gmail.com>
2022-08-30 14:24:17 +0000

    def print_list_integer(my_list=[]):

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,4 @@ def multiple_returns(sentence):
     if (len(sentence) == 0):
         return (0, None)
     else:
-        return (len(sentence),sentence[0])
+        return (len(sentence), sentence[0])
