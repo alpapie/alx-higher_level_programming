c5ba11796a0cff1f55dcdcab9ee8e9e49f28319a
alpapie <alpapie0908@gmail.com>
2022-08-30 13:05:17 +0000

    alpapie

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-import sys
-
-
 if __name__ == "__main__":
-    i = len(sys.argv) - 1
-    if i == 0:
+    import sys
+    total = len(sys.argv)
+    if total <= 1:
         print("0 arguments.")
-    elif i == 1:
-        print("1 argument:")
     else:
-        print("{} arguments:".format(i))
-    for n in range(i):
-        print("{}: {}".format(n+1, sys.argv[n+1]))
+        if total == 2:
+            print("{:d} argument:".format(total - 1))
+        else:
+            print("{:d} arguments:".format(total - 1))
+        for i in range(1, total):
+            print("{:d}: {}".format(i, sys.argv[i]))
