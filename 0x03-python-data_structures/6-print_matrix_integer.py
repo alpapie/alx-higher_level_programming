b9de3bc09a85dc4e054b2823f52f29be8a77ddd9
alpapie <alpapie0908@gmail.com>
2022-08-30 14:29:13 +0000

    def print_list_integer(my_list=[]):

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    print("\n".join([" ".join(["{:d}".format(i) for i in row]) for row in matrix]))
+    mat=[" ".join(["{:d}".format(i) for i in row]) for row in matrix]
+    print("\n".join(mat))
