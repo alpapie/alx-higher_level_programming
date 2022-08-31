c025d6c44a28cb17dffaa16640e2bff0a3a076b2
alpapie <alpapie0908@gmail.com>
2022-08-31 23:18:34 +0000

    alpapie the bosse

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-
 def roman_to_int(roman_string):
-    """Converts a roman numeral to an integer."""
-    roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+   
+    roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50, 
+                  "C": 100, "D": 500, "M": 1000}
     sum = 0
 
     if (not isinstance(roman_string, str) or
