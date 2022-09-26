cff81b61ea3d8aeb8808ad2904fbf0a38fbf4dc3
alpapie <alpapie0908@gmail.com>
2022-09-26 21:15:09 +0000

    inheritance classe python alx

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,15 @@
-#!/usr/bin/python3
+"""
+Returns the list of available attributes and methods of an object:
+"""
+
+
 def lookup(obj):
+    """retrun list of attributes and methods of `obj`
+
+    Args:
+        obj (Any): object
+
+    Returns:
+        list: list of attributes and members
+    """
     return dir(obj)
